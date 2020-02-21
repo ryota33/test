e37a12c86a37cdb26183707f96be076aefe888ad
ryota <ryota33@users.noreply.github.com>
2020-02-21 15:24:47 +0900

    first commit

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-"""
-このファイルに解答コードを書いてください
-"""
+with open('input.txt', mode='rt', encoding='utf-8') as f:
+  read_data = list(f)
+num = int(read_data[53])
+read_data.pop()
+sep = ":"
+ans = ""
+for i in read_data:
+  i = i[:-1]
+  i = i.split(sep)
+  i[0] = int(i[0])
+  if num%i[0]==0:
+    ans += i[1]
+print(ans)
